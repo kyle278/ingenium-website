817f5c8bf1be5228240b5169db0f7efbf4b31933
Kyle Redmond <kyle@ingeniumconsulting.net>
2026-05-12 09:36:40 +0100

    Add Google Tag Manager container

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Azeret_Mono, Manrope, Public_Sans } from "next/font/google";
+import Script from "next/script";
 
 import IngeniumTracking from "@/app/components/IngeniumTracking";
 import {
@@ -13,6 +14,15 @@ import {
 } from "@/lib/seo";
 
 import "./globals.css";
+
+const GTM_ID = "GTM-KWCQSXC7";
+const gtmInitScript = `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');
+`;
 
 const manropeDisplay = Manrope({
   subsets: ["latin"],
@@ -77,6 +87,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body
         className={`${manropeDisplay.variable} ${publicSansBody.variable} ${azeretMono.variable} min-h-screen bg-[var(--color-bg)] font-[var(--font-body)] text-[var(--color-text)] antialiased`}
       >
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {gtmInitScript}
+        </Script>
+        <noscript>
+          <iframe
+            height="0"
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+            width="0"
+          />
+        </noscript>
         <IngeniumTracking />
         {children}
       </body>
